3a761a2ad09b91024da54c140e68724cfff66764
electro898 <101351317+electro898@users.noreply.github.com>
2022-10-29 10:23:59 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -31,7 +31,7 @@ export default function Home({posts, events}) {
       </Head>
 
       <div className="container pt-5">
-        <h1 className="text-center pb-5">Positivityminds.com</h1>
+        <h1 className="text-center pb-5">Trendl24.com</h1>
         <div className="row">
           <div className="col-lg-8">
             <h2 className="pb-3">Recent Posts</h2>
